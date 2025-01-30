017ad1dd2ad18cc080567b7975ebdc7033b867bc
kuba-mazurkiewicz <kmazurki@cisco.com>
2025-01-30 14:46:35 +0100

    modified source to master branch

--- main.tf
+++ main.tf
@@ -10,11 +10,10 @@ provider "catalystcenter" {
   username    = "username"
   password    = "password"
   url         = "https://dnac.url"
-  max_timeout = 600
 }
 
 module "catalyst_center" {
-  source = "git::https://www.github.com/netascode/terraform-catalystcenter-nac-catalystcenter?ref=dev"
+  source = "git::https://www.github.com/netascode/terraform-catalystcenter-nac-catalystcenter.git"
 
   yaml_directories = ["data/"]
 }
